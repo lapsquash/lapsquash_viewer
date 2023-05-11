7bf709b7a1e6bee9aece814c1f29d3fc3012befe
ROTO0504 <0504hiroosa@gmail.com>
2023-05-11 11:30:14 +0900

    ページの追加

--- pages/projects.vue
+++ pages/projects.vue
@@ -4,6 +4,25 @@
 
 <template>
   <div>
-    <a>a</a>
+    <v-container>
+      <v-row>
+        <v-col>
+          <v-card>
+            <v-card-title>Projects</v-card-title>
+            <v-card-text>
+              <v-list>
+                <v-list-item-group>
+                  <v-list-item v-for="item in items" :key="item.title">
+                    <v-list-item-content>
+                      <v-list-item-title>{{ item.title }}</v-list-item-title>
+                    </v-list-item-content>
+                  </v-list-item>
+                </v-list-item-group>
+              </v-list>
+            </v-card-text>
+          </v-card>
+        </v-col>
+      </v-row>
+    </v-container>
   </div>
 </template>
