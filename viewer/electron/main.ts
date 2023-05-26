d850b81c4b167585f69ab78bce497617aed285d7
wappon-28-dev <wappon_28_dev@ruru.be>
2023-05-26 18:57:02 +0900

    ⬆️ nuxtとanalyzerの更新

--- viewer/electron/main.ts
+++ viewer/electron/main.ts
@@ -1,7 +1,5 @@
-/* eslint-disable dot-notation */
-import path from "path";
-
 import { app, BrowserWindow } from "electron";
+import path from "path";
 
 // The built directory structure
 //
@@ -13,15 +11,15 @@ import { app, BrowserWindow } from "electron";
 // │ ├─┬ renderer
 // │ │ └── index.html
 
-process.env["ROOT"] = path.join(__dirname, "..");
-process.env["DIST"] = path.join(process.env["ROOT"], "dist-electron");
-process.env["VITE_PUBLIC"] = process.env["VITE_DEV_SERVER_URL"]
-  ? path.join(process.env["ROOT"], "public")
-  : path.join(process.env["ROOT"], ".output/public");
+process.env.ROOT = path.join(__dirname, "..");
+process.env.DIST = path.join(process.env.ROOT, "dist-electron");
+process.env.VITE_PUBLIC = process.env.VITE_DEV_SERVER_URL
+  ? path.join(process.env.ROOT, "public")
+  : path.join(process.env.ROOT, ".output/public");
 process.env["ELECTRON_DISABLE_SECURITY_WARNINGS"] = "true";
 
 let win: BrowserWindow;
-const preload = path.join(process.env["DIST"], "preload.js");
+const preload = path.join(process.env.DIST, "preload.js");
 
 function bootstrap() {
   win = new BrowserWindow({
@@ -37,11 +35,11 @@ function bootstrap() {
     },
   });
 
-  if (process.env["VITE_DEV_SERVER_URL"]) {
-    win.loadURL(process.env["VITE_DEV_SERVER_URL"]);
+  if (process.env.VITE_DEV_SERVER_URL) {
+    win.loadURL(process.env.VITE_DEV_SERVER_URL);
     win.webContents.openDevTools();
   } else {
-    win.loadFile(path.join(process.env["VITE_PUBLIC"]!, "index.html"));
+    win.loadFile(path.join(process.env.VITE_PUBLIC!, "index.html"));
   }
 }
 
--- viewer/nuxt.config.ts
+++ viewer/nuxt.config.ts
@@ -1,6 +1,24 @@
 import { defineNuxtConfig } from "nuxt/config";
 import vuetify from "vite-plugin-vuetify";
 export default defineNuxtConfig({
+  modules: ["nuxt-electron"],
+  electron: {
+    build: [
+      {
+        // Main-Process entry file of the Electron App.
+        entry: "electron/main.ts",
+      },
+      {
+        entry: "electron/preload.ts",
+        onstart(options) {
+          // Notify the Renderer-Process to reload the page when the Preload-Scripts build is complete,
+          // instead of restarting the entire Electron App.
+          options.reload();
+        },
+      },
+    ],
+    renderer: {},
+  },
   build: {
     transpile: ["vuetify"],
   },
@@ -8,10 +26,6 @@ export default defineNuxtConfig({
     "vite:extendConfig": (config) => {
       config.plugins?.push(vuetify());
     },
-  },
-  modules: ["nuxt-electron"],
-  electron: {
-    renderer: {},
   },
   vite: {
     ssr: {
