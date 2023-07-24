ff3f6405c0d51f604809fb9491fb8bc3759f6b4e
ROTO0504 <0504hiroosa@gmail.com>
2023-07-25 01:25:00 +0900

    配置整え

--- viewer/src/pages/projects/[uuid].vue
+++ viewer/src/pages/projects/[uuid].vue
@@ -8,28 +8,44 @@ console.log(router.params.uuid);
     <p>プロジェクトID: {{ $route.params["uuid"] }}</p>
     <v-container>
       <v-row>
-        <v-col cols="12" xs="6" sm="6" md="4" lg="4" xl="3">
-          <v-card>
-            <v-card-title>説明</v-card-title>
-            <v-card-text>ここはプロジェクトの説明です。</v-card-text>
-          </v-card>
-        </v-col>
-        <v-row>
-          <v-col>
-            <v-col cols="12" xs="6" sm="6" md="4" lg="4" xl="3">
-              <v-card>
-                <v-card-title>アクション</v-card-title>
-                <v-card-text>行動の説明</v-card-text>
+        <v-col>
+          <v-row>
+            <v-col cols="12" xs="12" sm="12" md="12" lg="8" xl="8">
+              <v-card
+              class="video-player">
+                <video
+                  src="../../assets/sample/assets/0.mp4"
+                  autoplay="true"
+                  loop="true"
+                  muted
+                  controls
+                ></video>
               </v-card>
             </v-col>
-            <v-col cols="12" xs="6" sm="6" md="4" lg="4" xl="3">
-              <v-card>
-                <v-card-title>タグ</v-card-title>
-                <v-tab>a</v-tab>
-              </v-card>
-            </v-col>
+          </v-row>
+          <v-col cols="12" xs="6" sm="6" md="4" lg="4" xl="3">
+            <v-card>
+              <v-card-title>説明</v-card-title>
+              <v-card-text>ここはプロジェクトの説明です。</v-card-text>
+            </v-card>
           </v-col>
-        </v-row>
+          <v-row>
+            <v-col>
+              <v-col cols="12" xs="6" sm="6" md="4" lg="4" xl="3">
+                <v-card>
+                  <v-card-title>アクション</v-card-title>
+                  <v-card-text>行動の説明</v-card-text>
+                </v-card>
+              </v-col>
+              <v-col cols="12" xs="6" sm="6" md="4" lg="4" xl="3">
+                <v-card>
+                  <v-card-title>タグ</v-card-title>
+                  <v-tab>a</v-tab>
+                </v-card>
+              </v-col>
+            </v-col>
+          </v-row>
+        </v-col>
       </v-row>
     </v-container>
   </div>
@@ -41,5 +57,14 @@ console.log(router.params.uuid);
 }
 .v-card-text {
   font-size: 16px;
+}
+
+.video {
+  object-fit: cover;
+}
+
+.video-player {
+  aspect-ratio: 16/9;
+  z-index: 1;
 }
 </style>
